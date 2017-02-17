6d9cb5cb6ce69b6a926304b5051e1772e4d506be
Matthias Harmuth <mharmuth@brainbits.net>
2017-02-17 14:47:00 +0100

    Add listener test

---
+++ Tests/EventListener/NodeListenerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Phlexible\Bundle\NodeConnectionBundle\Tests\EventListener;
+
+use Phlexible\Bundle\NodeConnectionBundle\Doctrine\NodeConnectionManager;
+use Phlexible\Bundle\NodeConnectionBundle\EventListener\NodeListener;
+use Phlexible\Bundle\TreeBundle\Entity\TreeNode;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Phlexible\Bundle\ElementBundle\Event\SaveNodeDataEvent;
+use Phlexible\Bundle\ElementBundle\ElementEvents;
+
+class NodeListenerTest extends TestCase
+{
+    private $connection;
+
+    public function setUp()
+    {
+        $this->connections = [];
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $this->assertSame(
+            array(
+                ElementEvents::SAVE_NODE_DATA => array(array('onSaveNodeData', 15)),
+            ),
+            NodeListener::getSubscribedEvents()
+        );
+    }
+
+//    public function testOnSaveNodeData()
+//    {
+//        $request = new Request();
+//
+//        $treeNode = $this->prophesize(TreeNode::class);
+//        $treeNode->getId()->willReturn(10);
+//
+//        $event = new SaveNodeDataEvent(
+//            $treeNode,
+//            'de',
+//            $request
+//        );
+//
+//
+//
+//        $nodeConnectionManager = $this->prophesize(NodeConnectionManager::class);
+//        $nodeConnectionManager->findByNodeId()->willReturn();
+//
+//        $nodeListener = new NodeListener($nodeConnectionManager);
+//
+//
+//    }
+}
